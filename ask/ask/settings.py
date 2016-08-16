5f04f6b12b2c84c664fe9e405cb541b3d0e24ee6
romanyakovlev <yakovlevroman8@gmail.com>
2016-08-16 16:04:45 +0700

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -76,13 +76,10 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dbfordjango',
-        'USER': 'admin',
-        'PASSWORD': 'pass',
+        'ENGINE': 'django.db.backends.sqlite3',
+	'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
